2039ee7317060fec435d1b4c4a7e0827f00346b2
Tovpeko Sergey <aquadefin@gmail.com>
2024-04-28 22:55:49 +0300

    fix naming

--- server/main.py
+++ server/main.py
@@ -1,15 +1,16 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from endpoints.city import city
+from endpoints.venue import venue_router
+from endpoints.city import city_router
 from endpoints.global_event import global_event
 from endpoints.user import user_router
 
 app = FastAPI()
 app.include_router(user_router)
-app.include_router(city)
+app.include_router(city_router)
 app.include_router(global_event)
-
+app.include_router(venue_router)
 
 @app.get("/")
 async def root():
