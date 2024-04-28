571223f7a18e88134c8aafc7362c0a5d35e98a81
Tovpeko Sergey <aquadefin@gmail.com>
2024-04-28 19:08:31 +0300

    city

--- server/main.py
+++ server/main.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+
+from endpoints.city import city
+from endpoints.global_event import global_event
 from endpoints.user import user_router
 
 app = FastAPI()
 app.include_router(user_router)
+app.include_router(city)
+app.include_router(global_event)
 
 
 @app.get("/")
