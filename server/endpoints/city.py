dbc83f3b18300c694394a84597d0dedce35bca38
Tovpeko Sergey <aquadefin@gmail.com>
2024-04-28 22:56:03 +0300

    rename router

--- server/endpoints/city.py
+++ server/endpoints/city.py
@@ -6,22 +6,22 @@ import services
 from core.database import get_session
 from schemas.city import CityResponse, CityBaseResponse
 
-city = APIRouter(prefix="/api/city", tags=["City"])
+city_router = APIRouter(prefix="/api/city", tags=["City"])
 
 
-@city.get("/", response_model=list[CityResponse])
+@city_router.get("/", response_model=list[CityResponse])
 async def read_cities(db: AsyncSession = Depends(get_session)) -> list[CityResponse]:
     cities = await services.read_cities(db)
     return cities
 
 
-@city.post("/", response_model=CityBaseResponse)
+@city_router.post("/", response_model=CityBaseResponse)
 async def add_city(db: AsyncSession = Depends(get_session), city_name: str = "Moscow") -> CityBaseResponse:
     id_city = await services.add_city(db, city_name)
     return CityBaseResponse(id=id_city)
 
 
-@city.delete("/", response_model=CityBaseResponse)
+@city_router.delete("/", response_model=CityBaseResponse)
 async def delete_city(db: AsyncSession = Depends(get_session), city_id: int = 1) -> CityBaseResponse:
     id_city = await services.delete_city(db, city_id)
     if id_city is None:
